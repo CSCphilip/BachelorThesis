38ee6b120f2fe2ac7340ddee65569d84293b5114
Philip Andersson <phander@kth.se>
2020-03-13 15:03:05 +0100

    split files into save and load by using HDF5 formating

--- evaluate_model.py
+++ evaluate_model.py
@@ -8,68 +8,31 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Sleep for graphs
-import time
+# Load model
+model = tf.keras.models.load_model('saved_model.h5')
+#print(model.summary())
 
+# Load dataset
 fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+train_images = train_images / 255.0
+test_images = test_images / 255.0
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-# print(train_images.shape)
-# print(len(train_labels))
-# print(train_labels[0])
-# print(class_names[train_labels[0]])
-# print(train_labels)
-# print(test_images.shape)
-# print(len(test_labels))
-
-# plt.figure()
-# plt.imshow(train_images[0])
-# plt.colorbar()
-# plt.grid(False)
-# plt.show()
-
-train_images = train_images / 255.0
-test_images = test_images / 255.0
-
-# plt.figure(figsize=(10,10))
-# for i in range(25):
-#     plt.subplot(5,5,i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(train_images[i], cmap=plt.cm.binary)
-#     plt.xlabel(class_names[train_labels[i]])
-# plt.show()
-
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10)
-])
-
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-model.fit(train_images, train_labels, epochs=10)
-
+# Evaluate on testset
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
-print('\nTest accuracy:', test_acc)
+print('Test accuracy:', test_acc)
 
-probability_model = tf.keras.Sequential([model,
-                                         tf.keras.layers.Softmax()])
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 
 predictions = probability_model.predict(test_images)
 
 print(predictions[0])
 print(np.argmax(predictions[0]))
 print(np.argmax(predictions[0]) == test_labels[0])
-
-
 
 def plot_image(i, predictions_array, true_label, img):
   predictions_arr, true_label, img = predictions_array, true_label[i], img[i]
@@ -104,19 +67,17 @@ def plot_value_array(i, predictions_array, true_label):
 
 # Plot the first X test images, their predicted labels, and the true labels.
 # Color correct predictions in blue and incorrect predictions in red.
-# num_rows = 5
-# num_cols = 5
-# num_images = num_rows*num_cols
-# plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-# for i in range(num_images):
-#   plt.subplot(num_rows, 2*num_cols, 2*i+1)
-#   plot_image(i, predictions[i], test_labels, test_images)
-#   plt.subplot(num_rows, 2*num_cols, 2*i+2)
-#   plot_value_array(i, predictions[i], test_labels)
-# plt.tight_layout()
-# plt.show()
-
-
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
 
 # Grab an image from the test dataset.
 img = test_images[1]
@@ -131,5 +92,6 @@ print(predictions_single)
 
 plot_value_array(1, predictions_single[0], test_labels)
 _ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
 
 print(np.argmax(predictions_single[0]))
