1407c7fc1163e4b9b660cf2b5fee8a28d8b433a1
Linda Andersen <andersenlinda97@gmail.com>
2020-04-25 17:15:46 +0200

    added histogram equalization algorithm

--- dr_grading/build_with_histogram_equalization.py
+++ dr_grading/build_with_histogram_equalization.py
@@ -29,17 +29,59 @@ for i in range(1, input_images):
 X_train = np.zeros((len(train_ids), img_width, img_height,
         img_channels), dtype=np.float32) # dtype=np.uint8
 
+# Histogram equalization algorithm, from:
+# https://towardsdatascience.com/histogram-equalization-a-simple-way-to-improve-the-contrast-of-your-image-bcd66596d815
+def histogram_equalization(img_in):
+# segregate color streams
+    b,g,r = cv2.split(img_in)
+    h_b, bin_b = np.histogram(b.flatten(), 256, [0, 256])
+    h_g, bin_g = np.histogram(g.flatten(), 256, [0, 256])
+    h_r, bin_r = np.histogram(r.flatten(), 256, [0, 256])
+# calculate cdf    
+    cdf_b = np.cumsum(h_b)  
+    cdf_g = np.cumsum(h_g)
+    cdf_r = np.cumsum(h_r)
+    
+# mask all pixels with value=0 and replace it with mean of the pixel values 
+    cdf_m_b = np.ma.masked_equal(cdf_b,0)
+    cdf_m_b = (cdf_m_b - cdf_m_b.min())*255/(cdf_m_b.max()-cdf_m_b.min())
+    cdf_final_b = np.ma.filled(cdf_m_b,0).astype('uint8')
+  
+    cdf_m_g = np.ma.masked_equal(cdf_g,0)
+    cdf_m_g = (cdf_m_g - cdf_m_g.min())*255/(cdf_m_g.max()-cdf_m_g.min())
+    cdf_final_g = np.ma.filled(cdf_m_g,0).astype('uint8')
+    cdf_m_r = np.ma.masked_equal(cdf_r,0)
+    cdf_m_r = (cdf_m_r - cdf_m_r.min())*255/(cdf_m_r.max()-cdf_m_r.min())
+    cdf_final_r = np.ma.filled(cdf_m_r,0).astype('uint8')
+# merge the images in the three channels
+    img_b = cdf_final_b[b]
+    img_g = cdf_final_g[g]
+    img_r = cdf_final_r[r]
+  
+    img_out = cv2.merge((img_b, img_g, img_r))
+# validation
+    equ_b = cv2.equalizeHist(b)
+    equ_g = cv2.equalizeHist(g)
+    equ_r = cv2.equalizeHist(r)
+    equ = cv2.merge((equ_b, equ_g, equ_r))
+    #print(equ)
+    #cv2.imwrite('output_name.png', equ)
+    return img_out
+
 print("Preprocess the training images")
 for n, id in tqdm(enumerate(train_ids), total=len(train_ids)):
     input_img = cv2.imread(training_img_path + id + '.jpg')
     input_img = cv2.resize(input_img, (img_width, img_height))
     input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB) # Convert from BGR to RGB
+    input_img = histogram_equalization(input_img)
     input_img = input_img / 255.0 # Normalization
     X_train[n] = input_img
 print("Done!")
 
-# plt.imshow(X_train[0])
-# plt.show()
+
+for i in range(10):
+    plt.imshow(X_train[i])
+    plt.show()
 
 csv_batch_size = input_images - 1
 label_name = 'Retinopathy grade'
@@ -123,10 +165,9 @@ model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=Tru
 model.summary()
 
 batch_size = 16
-epochs = 15
+epochs = 30
 
-# TODO: crop input images to reduce noise
-# TODO: train on the full dataset, now no images with 0 grading exists
+#TODO: crop input images to reduce noise
 
 history = model.fit(X_train_tensor, labels, batch_size, epochs, validation_split=0.1)
 
